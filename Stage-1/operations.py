59cc3046313fef5093cab46dd99a0258121561d4
gbolaga-daramola <gbolagadaramola765@gmail.com>
2025-10-22 02:56:20 +0100

    Add string properties operations

---
+++ Stage-1/operations.py
@@ -0,0 +1,53 @@
+from collections import Counter 
+from datetime import datetime, timezone
+import hashlib
+
+def length(item: str):
+    '''
+    Returns number of characters in string
+    '''
+    return len(item)
+
+def is_palindrome(item: str):
+    """
+    Return true if words is reads backwards as same as forward 
+    otherswise returns false.
+    """
+    i, j = 0, len(item)-1
+    while i < j:
+        if item[i] != item[j]:return False 
+        i += 1
+        j -= 1
+    return True
+
+def unique_characters(item: str):
+    '''
+    Return count of unique characters in string
+    '''
+    return len(set(item))
+
+def word_count(item: str):
+    '''
+    Return number of words in item seperated by whitespace
+    '''
+    return len(item.split())
+
+def sha256_hash(item: str):
+    '''
+    Returns SHA-256 hash of item for unique identification
+    '''
+
+    input_bytes = item.encode("utf-8")
+    sha256_hash_object = hashlib.sha256(input_bytes)
+    hex_digest = sha256_hash_object.hexdigest()
+
+    return hex_digest
+
+def character_frequency_map(item: str):
+    """
+    Returns dictionary of each character as key and count as value
+    """
+    return dict(Counter(item))
+
+def get_current_time():
+    return datetime.now(timezone.utc)
